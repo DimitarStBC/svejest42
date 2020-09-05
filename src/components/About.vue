c564154d24008d9dc0dbd3b9dbc2e915cd6d636e
Dimitar Stoev <dimitarg.stoev@gmail.com>
2020-09-05 13:44:41 +0300

    finish

---
+++ src/components/About.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <h2 class="aboutBA">About</h2>
+    <div>
+      <div>
+        <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt voluptatum aliquid voluptatem cum dolores ratione. Veniam voluptatem ex impedit a!</h1>
+      </div>
+      <div></div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.aboutBA::before {
+  margin: 5px;
+  content: "";
+  width: 50px;
+  height: 2px;
+  background: #3498db;
+  display: inline-block;
+}
+.aboutBA::after {
+  margin: 5px;
+  content: "";
+  width: 50px;
+  height: 2px;
+  background: #3498db;
+  display: inline-block;
+}
+</style>
---
+++ src/components/Navigation.vue
@@ -0,0 +1,48 @@
+<template>
+  <ul class="nav nav-pills flex-column flex-sm-row">
+    <li class="nav-item navbar-brand">
+      <router-link class="nav-link" :to="{
+            name: 'Landing'
+              }">
+        <img src="@/assets/logo.png" width="50" alt />
+      </router-link>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="#about">About</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="#">Pricing</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="#">Contact</a>
+    </li>
+    <li class="nav-item">
+      <router-link class="nav-link" :to="{
+            name: 'Blog'
+              }">Blog</router-link>
+    </li>
+    <li class="nav-item">
+      <router-link class="nav-link" :to="{
+            name: 'Services'
+              }">Services</router-link>
+    </li>
+
+    <LanguageSwitcher />
+  </ul>
+</template>
+
+ <script>
+import LanguageSwitcher from "../components/LanguageSwitcher";
+
+export default {
+  components: {
+    LanguageSwitcher,
+  },
+};
+</script>
+
+<style scoped>
+.nav {
+  margin: 20px 0;
+}
+</style>
